NethServer/ns8-webtop

commit bd6923a98ace285f6119bbb5df088e02e7a29238

Switch ESLint parser to @babel/eslint-parser (#220)
fix https://github.com/NethServer/ns8-webtop/pull/184

ui/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: " @babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",

commit 8405fd89b6d1dc5f0746c5ca198b0424cc39e171

fix(regression): introduced in bd6923a

ui/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: " @babel/eslint-parser",
+    parser: "@babel/eslint-parser",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",