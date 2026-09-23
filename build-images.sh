f712c36b2d11e2c6a120886451cda1687aa095de
Stephane de Labrusse <stephdl@de-labrusse.fr>
2026-09-23 10:50:16 +0200

    chore: bump pecbridge to 5.4.11

--- build-images.sh
+++ build-images.sh
@@ -14,8 +14,9 @@ webtop_version=$(cat ${PWD}/webtop5-build/VERSION)
 
 # Download of external deps and CHECKSUM verification:
 if ! compgen -G "pecbridge-*.tar.gz"; then
-    curl --netrc --fail -O "https://www.sonicle.com/nethesis/commercial/pecbridge/pecbridge-5.4.8.tar.gz"
+    curl --netrc --fail -O "https://www.sonicle.com/nethesis/commercial/pecbridge/pecbridge-5.4.11.tar.gz"
 fi
+sha256sum pecbridge-*.tar.gz
 sha256sum -c CHECKSUM
 
 # Reuse existing webtopbuilder container, to speed up builds
